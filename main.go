31df488ae2692f12c5b951e7b1490f89961c4888
Manthan Sharma <manthan0908sharma@gmail.com>
2024-07-25 02:56:03 +0530

    add database & models

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/thedevsaddam/renderer"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName 		string = "localhost:27017"
+	dbName 			string = "golang_todo"
+	collectionName 	string = "todo"
+	port 			string = ":9000"
+)
+
+type (
+	todoModel struct {
+		ID 			bson.ObjectId 	`bson:"_id,omitempty"`
+		Title 		string			`bson:"title"`
+		Completed 	bool			`bson:"completed"`
+		CreatedAt	time.Time		`bson:"createdAt"`
+	}
+
+	todo struct {
+		ID 			bson.ObjectId 	`json:"id,omitempty"`
+		Title 		string			`json:"title"`
+		Completed 	bool			`json:"completed"`
+		CreatedAt	time.Time		`json:"created_at"`
+	}
+)
+
+func init()  {
+	rnd = renderer.New()
+
+	sess,err := mgo.Dial(hostName)
+	
+	checkErr(err)
+	
+	sess.SetMode(mgo.Monotonic,true)
+
+	db = sess.DB(dbName)
+}
+
+func checkErr(err error)  {
+	
+}
+
+func main() {
+	
 }
